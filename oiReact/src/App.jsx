ad35bfdce4e2f36bec4da97a97124abd280bf2bd
rinmarys <rinmarysmonagas@gmail.com>
2024-08-23 17:16:32 -0300

    Criação do primeiro projeto com react sobre dinnosauros beeeeem loucos_Embonitando com react

---
+++ oiReact/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+       <h1>Oi mundo</h1>
+       <h2>Oi mundo de dinossauros</h2>
+       <h2>Dinossauros carnivoros rawr</h2>
+
+       <p>TEXTO BEEEEEEEM LOUCOOOOOO 😨😧😧😨😨😱😰😱💀💀💀🤘🤘🤘🤘🤘🤘🤘</p>
+
+       <button>BOTÃO</button>
+    </>
+  )
+}
+
+export default App
